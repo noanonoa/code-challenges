44190998bcafd1415c0438b9982f828ebb361c4a
Noa Kim <noanonoa@gmail.com>
2020-12-30 10:28:41 -0800

    first duplicate value solution

---
+++ first-duplicate-value.js
@@ -0,0 +1,30 @@
+//PROMPT
+//Given an array of integers between 1 and n, inclusive, where n is the length of the array, write a function that returns the first integer that appears more than once (when the array is read from left to right).
+
+//In other words, out of all the integers that might occur more than once in the input array, your function should return the one whose first duplicate value has the minimum index.
+
+//If no integer appears more than once, your function should return -1.
+
+//NOTE
+//You're allowed to mutate the input array.
+
+//SAMPLE
+//INPUT:    array = [2, 1, 5, 2, 3, 3, 4]
+//OUTPUT:   int = 2
+
+//INPUT 2:  array = [2, 1, 5, 3, 3, 2, 4]
+//OUTPUT 2:  int = 3
+
+//SOLUTION
+function firstDuplicateValue(array) {
+  // Write your code here.
+  let hash = {};
+  for (let i = 0; i < array.length; i++) {
+    let num = array[i]
+    if (num in hash) {
+      return num
+    }
+    hash[num] = (hash[num] || 0) + 1
+  }
+  return -1;
+}
