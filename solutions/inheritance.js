8e7b4730edd7c928eb50f836d4d5e85b78812486
Noa Kim <noanonoa@gmail.com>
2021-05-13 19:29:15 -0700

    Inheritance initial prompt

---
+++ solutions/inheritance.js
@@ -0,0 +1,25 @@
+/**
+ * In this challenge, we practice implementing inheritance and use JavaScript prototypes to add a new method to an existing prototype.
+ * Provided is the implementation for a Rectangle class.  Perform the following tasks:
+ * 
+ * 1. Add an area method to Rectangle's prototype
+ * 2. Create a Square class that satisfies the following:
+ *  - It is a subclass of Rectangle.
+ *  - It contains a constructor and no other methods.
+ *  - It can use the Rectangle class' area method to print the area of a Square object.
+ */
+
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+  /**
+   * Write code that adds an 'area' method to the Rectangle class' prototype
+   */
+
+  /**
+   * Create a Square class that inherits from Rectangle and implement its class constructor
+   */
+
+};
