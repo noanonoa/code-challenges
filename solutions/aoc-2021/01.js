40dc9cab95dffe0422da984eaa90eb0842b9fa79
Noa Kim <noanonoa@gmail.com>
2021-12-10 09:21:33 -0600

    Day 1 Part 2 solution

--- solutions/aoc-2021/01.js
+++ solutions/aoc-2021/01.js
@@ -20,3 +20,23 @@ function countIncrements(arr){
 }
 
 console.log(countIncrements(data)) // 1466
+
+/**
+ * Part Two
+ */
+
+function countSlidingWindow(arr){
+  // type casting from String to Number
+  let measurements = arr.map(d => Number(d)),
+    slidingWindow = measurements[0] + measurements[1] + measurements[2],
+    slidingWindowIncrements = 0
+  for (let i = 1; i < measurements.length - 2; i++){
+    if (measurements[i] + measurements[i+1] + measurements[i+2] > slidingWindow){
+      slidingWindowIncrements++
+    }
+    slidingWindow = measurements[i] + measurements[i+1] + measurements[i+2]
+  }
+  return slidingWindowIncrements
+}
+
+console.log(countSlidingWindow(data)) // 1491
