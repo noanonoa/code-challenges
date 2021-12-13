dcf21792d5eecc42c656c497d2ccced976927a94
Noa Kim <noanonoa@gmail.com>
2021-12-12 19:28:43 -0600

    plus minus prompt

---
+++ solutions/plus-minus.js
@@ -0,0 +1,14 @@
+/**
+ * Prompt
+ * 
+ * Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
+ * 
+ * Example:
+ * arr = [1, 1, 0, -1, -1]
+ * 
+ * There are n = 5 elements, two positive, two negative, and one zero.  Their ratios are 2/5 = 0.400000 and 2/5 = 0.400000 and 1/5 = 0.200000
+ * Results are printed as: 
+ * 0.400000
+ * 0.400000
+ * 0.200000
+ */
