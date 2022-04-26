e329db4c1af7881fa3ebdba604ad1a9a0d2eee33
Noa Kim <noanonoa@gmail.com>
2022-04-26 17:15:59 -0500

    cockroach solution refactor

--- solutions/solutions_in_ruby/cockroach_speed.rb
+++ solutions/solutions_in_ruby/cockroach_speed.rb
@@ -5,8 +5,9 @@
 #
 # Note: The input is a Real number and is >= 0.  The result should be an integer.
 
-def cockroach_speed(s)
-  speed_converted_to_centimeter_per_second = s * 100000 / 3600
-
-  speed_converted_to_centimeter_per_second.floor
+def cockroach_speed(km_per_hour)
+  CM_PER_KM = 100000
+  SECONDS_PER_HOUR = 3600
+  cm_per_second = km_per_hour * CM_PER_KM / SECONDS_PER_HOUR
+  cm_per_second.floor
 end
