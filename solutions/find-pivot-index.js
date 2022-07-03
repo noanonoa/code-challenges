6c39912ac652dcda5c65751bc7a29a9b4a04093f
Noa Kim <noanonoa@gmail.com>
2022-07-03 18:25:02 -0500

    add logs

--- solutions/find-pivot-index.js
+++ solutions/find-pivot-index.js
@@ -49,4 +49,8 @@ function pivotIndex(nums) {
   return -1;
 };
 
+console.log('pivotIndex([1,7,3,6,5,6]):', pivotIndex([1,7,3,6,5,6])); // -> 3
+console.log('pivotIndex([1,2,3]):', pivotIndex([1,2,3])); // -> -1
+console.log('pivotIndex([2,1,-1]):', pivotIndex([2,1,-1])); // -> 0
+
 module.exports = pivotIndex;
