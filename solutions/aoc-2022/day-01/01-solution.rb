5584210c640c79236235a54cffcdabf9f6ece77c
Noa Kim <noanonoa@gmail.com>
2022-12-05 19:33:08 -0600

    aoc 2022 day 1 part 1

---
+++ solutions/aoc-2022/day-01/01-solution.rb
@@ -0,0 +1,13 @@
+puzzle_inputs = File.readlines('01-puzzle-input.csv').map { |element| element.to_i }
+results = []
+arr = []
+
+puzzle_inputs.each do |input|
+  if input == 0
+    results << arr.sum
+    arr = []
+  end
+  arr << input
+end
+
+puts results.max
