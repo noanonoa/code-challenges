306f3dccff932b1e1117e883bfc5c721382b71d6
Noa Kim <noanonoa@gmail.com>
2022-12-06 22:52:56 -0600

    aoc 2022 day 2 part 2

--- solutions/aoc-2022/day-02/02-solution.rb
+++ solutions/aoc-2022/day-02/02-solution.rb
@@ -23,12 +23,11 @@
 # 3rd round: draw = (3) draw + (3) scissors =  6 points
 #
 #                               Total Score = 15 points
-
+#
 puzzle_inputs = File.readlines('02-puzzle-input.csv').map { |input| input[0] + input[2] }
-
-WINNING_COMBO = ["AY", "BZ", "CX"]
-DRAW_COMBO = ["AX", "BY", "CZ"]
-LOSE_COMBO = ["AZ", "BX", "CY"]
+winning_combo = ["AY", "BZ", "CX"]
+draw_combo = ["AX", "BY", "CZ"]
+lose_combo = ["AZ", "BX", "CY"]
 
 win_rounds = 0
 draw_rounds = 0
@@ -37,9 +36,9 @@ points = 0
 puzzle_inputs.each do |input|
   player_choice = input[1]
 
-  if WINNING_COMBO.include?(input)
+  if winning_combo.include?(input)
     win_rounds += 1
-  elsif DRAW_COMBO.include?(input)
+  elsif draw_combo.include?(input)
     draw_rounds += 1
   end
 
@@ -53,5 +52,63 @@ puzzle_inputs.each do |input|
 end
 
 total_score = (win_rounds * 6) + (draw_rounds * 3) + points
-puts total_score
+puts "part 1: #{total_score}"
 
+# part two
+# Now [X,Y,Z] = [lose, draw, win]
+#
+# example from part 1:
+# A Y => Rock     vs ? = draw => Rock (X) points: 1(X) + 3(D) = 4
+# B X => Paper    vs ? = lose => Rock (X) points: 1(X) + 0(L) = 1
+# C Z => Scissors vs ? = win  => Rock (X) points: 1(X) + 3(6) = 7
+#
+#                                                  Total Score= 12
+#
+winning_combo = ["AZ", "BZ", "CZ"]
+draw_combo = ["AY", "BY", "CY"]
+lose_combo = ["AX", "BX", "CX"]
+
+win_rounds = 0
+draw_rounds = 0
+points = 0
+
+puzzle_inputs.each do |input|
+  elf_choice, round_outcome = input.split('')
+
+  # win against elf
+  if winning_combo.include?(input)
+    win_rounds += 1
+    case elf_choice
+    when "A"
+      points += 2
+    when "B"
+      points += 3
+    else
+      points += 1
+    end
+  # draw against elf
+  elsif draw_combo.include?(input)
+    draw_rounds += 1
+    case elf_choice
+    when "A"
+      points += 1
+    when "B"
+      points += 2
+    else
+      points += 3
+    end
+  # lose against elf
+  else
+    case elf_choice
+    when "A"
+      points += 3
+    when "B"
+      points += 1
+    else
+      points += 2
+    end
+  end
+end
+
+total_score = (win_rounds * 6) + (draw_rounds * 3) + points
+puts "part 2: #{total_score}"
